c6937d960aa34f0cf4027e88a73a15ea4b2888b5
kkoston <chris@koston.us>
2013-06-13 17:36:43 -0300

    initial check-in

---
+++ go.sh
@@ -0,0 +1,7 @@
+sudo apt-get update && sudo apt-get install git-core curl build-essential openssl libssl-dev
+git clone https://github.com/joyent/node.git
+cd node
+git checkout v0.9.5 (or the version you want to build)
+./configure --openssl-libpath=/usr/lib/ssl
+make
+sudo make install
